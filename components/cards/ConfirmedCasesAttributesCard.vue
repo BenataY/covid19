5d6b68bba2305818b5b288ff3a7462755d90dc9a
yuta <benata.yu@gmail.com>
2020-04-18 10:54:09 +0900

    データの入手先を変更

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -7,7 +7,7 @@
       :chart-option="{}"
       :date="Data.patients.date"
       :info="sumInfoOfPatients"
-      :url="'https://catalog.data.metro.tokyo.lg.jp/dataset/t000010d0000000068'"
+      :url="'https://www.pref.ibaraki.jp/1saigai/2019-ncov/index.html'"
       :source="$t('オープンデータを入手')"
     />
   </v-col>

de57b46b386354bcef0b14962e96c19fc0111640
yuta <benata.yu@gmail.com>
2020-04-20 01:09:56 +0900

    動くようになった

--- components/CitiesBarChart.vue
+++ components/CitiesBarChart.vue
@@ -1,26 +1,105 @@
 <template>
-  <data-view :title="title" :title-id="titleId" :date="date" :url="url">
-    <chart></chart>
+  <data-view :title="title" :title-id="titleId" :date="date">
     <template v-slot:infoPanel>
-      <data-view-basic-info-panel
-        :l-text="displayInfo.lText"
-        :s-text="displayInfo.sText"
-        :unit="displayInfo.unit"
-      />
+      <small :class="$style.DataViewDesc">
+        <slot name="description" />
+      </small>
     </template>
-    <template v-slot:footer>
-      <open-data-link v-show="url" :url="url" />
-    </template>
+    <h4 :id="`${titleId}-graph`" class="visually-hidden">
+      {{ $t(`{title}のグラフ`, { title }) }}
+    </h4>
+    <bar
+      :ref="'barChart'"
+      :style="{ display: canvas ? 'block' : 'none' }"
+      :chart-id="chartId"
+      :chart-data="displayData"
+      :options="displayOption"
+      :height="240"
+    />
+    <v-data-table
+      :style="{ top: '-9999px', position: canvas ? 'fixed' : 'static' }"
+      :headers="tableHeaders"
+      :items="tableData"
+      :items-per-page="-1"
+      :hide-default-footer="true"
+      :height="240"
+      :fixed-header="true"
+      :disable-sort="true"
+      :mobile-breakpoint="0"
+      class="cardTable"
+      item-key="name"
+    />
   </data-view>
 </template>
 
-<script>
-import DataView from '@/components/DataView.vue'
-import DataViewBasicInfoPanel from '@/components/DataViewBasicInfoPanel.vue'
-import OpenDataLink from '@/components/OpenDataLink.vue'
+<style module lang="scss">
+.DataView {
+  &Desc {
+    margin-top: 10px;
+    margin-bottom: 0 !important;
+    font-size: 12px;
+    color: $gray-3;
+  }
+}
+</style>
 
-export default {
-  components: { DataView, DataViewBasicInfoPanel, OpenDataLink },
+<script lang="ts">
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import { ChartOptions } from 'chart.js'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import Data from '@/data/data.json'
+import DataView from '@/components/DataView.vue'
+import { getGraphSeriesStyle } from '@/utils/colors'
+
+interface HTMLElementEvent<T extends HTMLElement> extends MouseEvent {
+  currentTarget: T
+}
+type Data = {
+  canvas: boolean
+  chartData: typeof Data.cities.data
+  date: string
+  cities: VueI18n.TranslateResult[]
+}
+type Methods = {}
+type Computed = {
+  displayData: {
+    labels: string[]
+    datasets: {
+      label: string
+      data: number[]
+      backgroundColor: string
+      borderColor: string
+      borderWidth: number
+    }[]
+  }
+  displayOption: ChartOptions
+  tableHeaders: {
+    text: VueI18n.TranslateResult
+    value: string
+  }[]
+  tableData: {
+    [key: number]: number
+  }[]
+}
+type Props = {
+  title: string
+  titleId: string
+  chartId: string
+  unit: string
+}
+
+const options: ThisTypedComponentOptionsWithRecordProps<
+  Vue,
+  Data,
+  Methods,
+  Computed,
+  Props
+> = {
+  created() {
+    this.canvas = process.browser
+  },
+  components: { DataView },
   props: {
     title: {
       type: String,
@@ -35,35 +114,146 @@ export default {
     chartId: {
       type: String,
       required: false,
-      default: 'chart'
-    },
-    chartData: {
-      type: Array,
-      required: false,
-      default: () => []
-    },
-    date: {
-      type: String,
-      required: true,
-      default: ''
+      default: 'cities-bar-chart'
     },
     unit: {
       type: String,
       required: false,
       default: ''
-    },
-    info: {
-      type: String,
-      required: false,
-      default: ''
-    },
-    url: {
-      type: String,
-      required: false,
-      default: ''
+    }
+  },
+  data() {
+    const cities = [
+      this.$t('陽性患者数')
+    ]
+    const citiesData = Data.cities.data
+    citiesData.datasets.map(dataset => {
+      dataset.label = this.$t(dataset.label) as string
+    })
+    return {
+      canvas: true,
+      chartData: citiesData,
+      date: Data.cities.date,
+      cities
     }
   },
   computed: {
+    displayData() {
+      const graphSeries = getGraphSeriesStyle(this.chartData.datasets.length)
+      return {
+        labels: this.chartData.labels as string[],
+        datasets: this.chartData.datasets.map((item, index) => {
+          return {
+            label: this.cities[index] as string,
+            data: item.data,
+            backgroundColor: graphSeries[index].fillColor,
+            borderColor: graphSeries[index].strokeColor,
+            borderWidth: 1
+          }
+        })
+      }
+    },
+    displayOption() {
+      const self = this
+      const options: ChartOptions = {
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+            title(tooltipItem) {
+              const dateString = tooltipItem[0].label
+              return self.$t('{duration}', {
+                duration: dateString!
+              }) as string
+            },
+            label(tooltipItem, data) {
+              const index = tooltipItem.datasetIndex!
+              const num = parseInt(tooltipItem.value!).toLocaleString()
+              const unit = self.$t(self.unit)
+              return `${num} ${unit}`
+            }
+          }
+        },
+        legend: {
+          display: false,
+          onHover: (e: HTMLElementEvent<HTMLInputElement>) => {
+            e.currentTarget!.style!.cursor = 'pointer'
+          },
+          onLeave: (e: HTMLElementEvent<HTMLInputElement>) => {
+            e.currentTarget!.style!.cursor = 'default'
+          },
+          labels: {
+            boxWidth: 20
+          }
+        },
+        scales: {
+          xAxes: [
+            {
+              stacked: true,
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontSize: 9,
+                fontColor: '#808080'
+              }
+            }
+          ],
+          yAxes: [
+            {
+              stacked: true,
+              gridLines: {
+                display: true
+              },
+              ticks: {
+                fontSize: 9,
+                fontColor: '#808080',
+                maxTicksLimit: 10,
+                callback(label) {
+                  return `${label}${self.unit}`
+                }
+              }
+            }
+          ]
+        }
+      }
+      if (this.$route.query.ogp === 'true') {
+        Object.assign(options, { animation: { duration: 0 } })
+      }
+      return options
+    },
+    tableHeaders() {
+      return [
+        { text: this.$t('日付'), value: 'text' },
+        ...this.displayData.datasets.map((text, value) => {
+          return { text: text.label, value: String(value) }
+        })
+      ]
+    },
+    tableData() {
+      return this.displayData.datasets[0].data.map((_, i) => {
+        return Object.assign(
+          { text: this.displayData.labels[i] as string },
+          ...this.displayData.datasets!.map((_, j) => {
+            return {
+              [j]: this.displayData.datasets[0].data[i]
+            }
+          })
+        )
+      })
+    }
+  },
+  mounted() {
+    const barChart = this.$refs.barChart as Vue
+    const barElement = barChart.$el
+    const canvas = barElement.querySelector('canvas')
+    const labelledbyId = `${this.titleId}-graph`
+
+    if (canvas) {
+      canvas.setAttribute('role', 'img')
+      canvas.setAttribute('aria-labelledby', labelledbyId)
+    }
   }
 }
+
+export default Vue.extend(options)
 </script>
--- components/cards/CitiesCard.vue
+++ components/cards/CitiesCard.vue
@@ -2,14 +2,13 @@
   <v-col cols="12" md="6" class="DataCard">
     <cities-bar-chart
       :title="$t('市町村毎の陽性患者数')"
-      :title-id="'patietns-and-cities'"
-      :chart-data="citiesGraph"
-      :date="date"
+      :title-id="'cities'"
+      :chart-id="'cities'"
+      :chart-data="citiesData"
+      :date="citiesData.date"
       :unit="$t('人')"
-      :info="$t('累計値')"
-      :url="'https://www.pref.ibaraki.jp/1saigai/2019-ncov/hassei.html'"
-      :source="$t('参考元データ')"
-    />
+    >
+    </cities-bar-chart>
   </v-col>
 </template>
 
@@ -21,32 +20,15 @@ export default {
   components: {
     CitiesBarChart
   },
-  data () {
+  data() {
+    const cities = [
+      this.$t('陽性患者数')
+    ]
+    const citiesData = Data.cities.data
     return {
-      date: String,
-      citiesGraph: [],
-      options: {
-        scales: {
-          xAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              stepSize: 10,
-            }
-          }]
-        }
-      }
+      citiesData,
+      cities
     }
-  },
-  mounted () {
-    this.date = Data.cities.date
-    citiesGraph = Data.cities.data
-    //this.renderChart(this.data, this.options)
   }
 }
 </script>

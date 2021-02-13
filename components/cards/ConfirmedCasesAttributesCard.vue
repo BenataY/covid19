510b81943f5a9242f41667f436a4c78b3802f011
BenataY <benata.yu@gmail.com>
2021-02-13 15:05:47 +0900

    エラー、警告が多いので、i18n適用を外す

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -42,24 +42,24 @@ export default {
     // 陽性患者の属性 ヘッダー翻訳
     for (const header of patientsTable.headers) {
       header.text =
-        header.value === '退院' ? this.$t('退院※') : this.$t(header.value)
+        header.value === '退院' ? '退院※' : header.value
     }
     // 陽性患者の属性 中身の翻訳
     for (const row of patientsTable.datasets) {
-      row['居住地'] = this.$t(row['居住地'])
-      row['性別'] = this.$t(row['性別'])
+      row['居住地'] = [row['居住地']]
+      row['性別'] = [row['性別']]
 
       if (row['年代'] === '10歳未満') {
-        row['年代'] = this.$t('10歳未満')
+        row['年代'] = '10歳未満'
       } else if (row['年代'] === '不明') {
-        row['年代'] = this.$t('不明')
+        row['年代'] = '不明'
       } else if (row['年代'] === '非公表') {
-        row['年代'] = this.$t('非公表')
+        row['年代'] = '非公表'
       } else if (row['年代'] === '調査中') {
-        row['年代'] = this.$t('調査中')
+        row['年代'] = '調査中'
       } else {
         const age = row['年代'].substring(0, 2)
-        row['年代'] = this.$t('{age}代', { age })
+        row['年代'] = ['{age}代', { age }]
       }
     }
 
